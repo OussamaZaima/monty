e3c14074502bc7da6924a796b428dfbb51e6d666
oussama zaima <oussama.zaima2002@gmail.com>
2023-12-14 17:12:53 -0800

    all task

--- push.c
+++ push.c
@@ -18,13 +18,13 @@ void f_push(stack_t **head, unsigned int counter)
 			if (bus.arg[j] > 57 || bus.arg[j] < 48)
 				flag = 1; }
 		if (flag == 1)
-		{ fprint(stderr, "L%d: usage: push integer\n", counter);
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
 			free(bus.content);
 			free_stack(*head);
 			exit(EXIT_FAILURE); }}
 	else
-	{ fprint(stderr, "L%d: usage: push integer\n", counter);
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
